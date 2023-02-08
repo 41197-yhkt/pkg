cb1ce84ffe68afbd12fbd6776f84adc61c91a52b
perhapzz <48175595+perhapzz@users.noreply.github.com>
2023-02-08 21:50:28 +0800

    refactor: rename compservicename

--- constants/constants.go
+++ constants/constants.go
@@ -3,7 +3,7 @@ package constants
 const (
 	SecretKey                = "secret key"
 	VideoServiceName         = "videoservice"
-	CompServiceName          = "compservice"
+	CompServiceName          = "composite"
 	UserServiceName          = "userservice"
 	MySQLDefaultDSN          = "root:123456@tcp(localhost:6666)/tiktok_db?charset=utf8&parseTime=True&loc=Local"
 	EtcdAddress              = "127.0.0.1:2379"
